e8fddd0affc9b60524d3c1db908c4ea340c65966
CLP <clpaustralia@gmail.com>
2017-08-20 08:37:26 +1000

    Added DB; Added blog index page & pagination

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
+use AppBundle\Entity\Comment;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Psr\Log\LoggerInterface;
 
 /**
  * @Route("/blog")
@@ -14,10 +18,20 @@ class BlogController extends Controller
 
   /**
    * @Route("/", defaults={"page": "1"}, name="blog_index")
+   * @Route("/page/{page}", requirements={"page": "[1-9]\d*"}, name="blog_index_paginated")
    * */
-  public function indexAction($page)
+  public function indexAction(LoggerInterface $logger, $page)
   {
-    return new Response("TODO: blog index");
+    $posts = $this->getDoctrine()->getRepository(Post::class)->findLatest($page);
+    $logger->info('Total posts retrieved: '.count($posts));
+    return $this->render('blog/index.html.twig', ['posts' => $posts] );
   }
 
+  /**
+   * @Route("/posts/{slug}", name="blog_post")
+   * */
+  public function postShowAction(Post $post)
+  {
+    return new Response("TODO: blog_post");
+  }
 }
