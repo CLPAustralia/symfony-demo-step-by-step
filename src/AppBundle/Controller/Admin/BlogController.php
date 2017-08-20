8ffb9f3c672b1b4b2bed24a9f2c7d1fcde4e39cd
CLP <clpaustralia@gmail.com>
2017-08-20 21:06:54 +1000

    Enabled logger service

--- src/AppBundle/Controller/Admin/BlogController.php
+++ src/AppBundle/Controller/Admin/BlogController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller\Admin;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Post;
 
 /**
  * @Route("/admin/post")
@@ -16,16 +19,18 @@ class BlogController
    * @Route("/", name="admin_index")
    * @Route("/", name="admin_post_index")
    * */
-  public function indexAction()
+  public function indexAction(LoggerInterface $logger)
   {
+    $logger->info("### Admin Blog Index");
     return new Response("TODO: admin index / admin post index");
   }
 
   /**
    * @Route("/{id}/edit", requirements={"id": "\d+"}, name="admin_post_edit")
    * */
-  public function editAction(Post $post, Request $request)
+  public function editAction(LoggerInterface $logger, Post $post, Request $request)
   {
+    $logger->info("### Admin Blog Edit");
     return new Response("TODO: admin post edit");
   }
 }
